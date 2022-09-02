fd656cdf6249d709eaaa4e144d52b91c59e5cdbd
khan <khanetmoi@gmail.com>
2022-09-02 23:49:21 +0000

    using Api to fetch the namse and the  scores

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import './styles/index.css';
+
+import { postData, getApiData } from './api.js';
+
+const resetBtn = document.querySelector('#refresh');
+const submit = document.querySelector('#submit');
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault();
+  postData();
+});
+
+resetBtn.addEventListener('click', getApiData);
+window.addEventListener('load', getApiData);
